b1d5554f097a2925aa11d3cff874f56960f59a52
Mike Caspar <mike@caspar.com>
2015-07-16 16:11:08 -0400

    README points to one Single Test

--- test/mochaSingleTests.js
+++ test/mochaSingleTests.js
@@ -14,12 +14,13 @@ describe("A Core Homepage Test", function () {
 
             resFinal = null;
 
+            var url = "http://www.caspar.com";
             var searchTag = "description";
             var searchType = "name";
             var content = "Caspar Computer Services Inc.  Providing Innovative Solutions since 1984. Passionate about Agile Development";
 
 
-            metaconfirm.FindInPage("http://www.caspar.com", searchType, searchTag, content, function callback(result) {
+            metaconfirm.FindInPage(url, searchType, searchTag, content, function callback(result) {
 
                 resFinal = result;
 
