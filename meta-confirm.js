432b5daa4e28813fa78d5569f1654ccb524396de
Mike Caspar <mike@caspar.com>
2015-07-17 12:01:27 -0400

    add .gitkeep for lib folder a bit of code cleanup

--- meta-confirm.js
+++ meta-confirm.js
@@ -6,11 +6,9 @@ var exports = module.exports = {};
 var request = require('request');
 
 
-function findInPage(complete_url, searchType, searchForTag, searchContent, cb) {
+function findInPage(complete_url, searchType, searchTag, searchContent, cb) {
 
-
-    if (complete_url === null)
-    {
+    if (complete_url === null) {
         cb(new Error("url cannot be null"), null);
         return;
     }
@@ -18,27 +16,27 @@ function findInPage(complete_url, searchType, searchForTag, searchContent, cb) {
 
     request(complete_url, function (error, response, body) {
 
-            if (error) {
-                cb(error, null);
+                if (error) {
+                    cb(error, null);
 
-            }
-            else {
-
-
-                var specificTag = '<meta ' + searchType + '="' + searchForTag + '" content="' + searchContent + '">';
-
-
-                var cnt = body.split(specificTag).length;
-
-                if (cnt === 1) {
-                    cb(null, false);
                 }
                 else {
-                    cb(null, true);
 
+
+                    var specificTag = '<meta ' + searchType + '="' + searchTag + '" content="' + searchContent + '">';
+
+
+                    var cnt = body.split(specificTag).length;
+
+                    if (cnt === 1) {
+                        cb(null, false);
+                    }
+                    else {
+                        cb(null, true);
+
+                    }
                 }
             }
-        }
     );
 
 }
