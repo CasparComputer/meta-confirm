db6281928da8e1adca5a150a0773ceaaf6570642
Mike Caspar <mike@caspar.com>
2015-07-23 09:43:33 -0400

    extract validations to a seperate js file for code simplicity and separation of concerns

--- meta-confirm.js
+++ meta-confirm.js
@@ -1,24 +1,17 @@
 // metaConfirm.js
 
 var exports = module.exports = {};
-
 var request = require('request');
+var validations = require('./lib/validations');
 
 
 function findInPage(completeUrl, searchMetaType, searchTag, searchContent, cb) {
     "use strict";
 
-    function sendBackAnError(cb, errorMessage) {
+    var nullErrorMessage = validations.checkForNullParameters(completeUrl, searchMetaType, searchTag, searchContent);
 
-        cb(new Error(errorMessage), null);
-
-
-    }
-
-    var errorString = checkForNullParameters(completeUrl, searchMetaType, searchTag, searchContent);
-
-    if (errorString !== null) {
-        sendBackAnError(cb, errorString);
+    if (nullErrorMessage !== null) {
+        cb(nullErrorMessage, null);
         return;
     }
 
@@ -46,31 +39,6 @@ function findInPage(completeUrl, searchMetaType, searchTag, searchContent, cb) {
 
 }
 
-
-function checkForNullParameters(completeUrl, searchMetaType, searchTag, searchContent) {
-    'use strict';
-
-    var errorString = null;
-
-    if (completeUrl === null) {
-        errorString = "url cannot be null";
-    }
-
-    if (searchMetaType === null) {
-        errorString = "searchMetaType cannot be null";
-    }
-
-    if (searchTag === null) {
-        errorString = "searchTag cannot be null";
-    }
-
-    if (searchContent === null) {
-        errorString = "searchContent cannot be null";
-    }
-
-    return errorString;
-
-}
 
 function countMatchesInString(body, searchMetaType, searchTag, searchContent) {
     "use strict";
